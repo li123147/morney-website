41e5540dc54e7cbeec4228e5caf1ed61eb063a4e
lixiaohu123147 <7595644+lixiaohu123147@user.noreply.gitee.com>
2020-07-06 22:13:31 +0800

    init

---
+++ precache-manifest.fc2dd8ec815b5897e02d2c01fa78e85d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7fa1e1d5eea083dc7e00beff85468b",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "5b8239d2c0bd0ee020b4",
+    "url": "/morney-website/static/css/main.e22725fa.chunk.css"
+  },
+  {
+    "revision": "7adeb357e32c06af4dc1",
+    "url": "/morney-website/static/js/2.3ce90c16.chunk.js"
+  },
+  {
+    "revision": "99406820fc9e02f862dbd66e126ca114",
+    "url": "/morney-website/static/js/2.3ce90c16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8239d2c0bd0ee020b4",
+    "url": "/morney-website/static/js/main.b4681f4d.chunk.js"
+  },
+  {
+    "revision": "97b3b0c0492b50cb5532",
+    "url": "/morney-website/static/js/runtime-main.699989c5.js"
+  }
+]);
